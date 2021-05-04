221d1b40240bded9ec6b335a47dd1b440b57a5f5
Mejotandrewt <79672003+Mejotandrewt@users.noreply.github.com>
2021-05-03 22:41:46 -0400

    fixing button

--- script/script.js
+++ script/script.js
@@ -2,9 +2,9 @@ let btnRace = document.querySelector('button');
 let results = document.querySelector('h1');
 
 
-let race = ['Human', "Elf", "Dwarf", "Halfling", "Dragonborn", "Half-Elf", "Gnome"]
+let creature = ['Human', "Elf", "Dwarf", "Halfling", "Dragonborn", "Half-Elf", "Gnome"];
 
-function getRandomNumber(min, max){
+function getRandomNumber(min, max) {
     let step1 = max - min + 1;
     let step2 = Math.random() * step1;
     let result = Math.floor(step2) + min;
@@ -14,6 +14,6 @@ function getRandomNumber(min, max){
 }
 
 btnRace.addEventListener('click', () => {
-    let index = getRandomNumber(0, race.length-1)
-    result.innerText =race[index];
+    let index = getRandomNumber(0, creature.length-1)
+    result.innerText =creature[index];
 });
