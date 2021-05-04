bb8a44f7f0b9ffbd33e34a4039bcda6ad6fa99f9
Mejotandrewt <79672003+Mejotandrewt@users.noreply.github.com>
2021-05-03 22:31:35 -0400

    Trying to add random button selector

---
+++ script/script.js
@@ -0,0 +1,19 @@
+let btnRace = document.querySelector('button');
+let results = document.querySelector('h1');
+
+
+let race = {'Human', "Elf", "Dwarf", "Halfling", "Dragonborn", "Half-Elf", "Gnome"}
+
+function getRandomNumber(min, max){
+    let step1 = max - min + 1;
+    let step2 = Math.random() * step1;
+    let result = Math.floor(step2) + min;
+
+    return result;
+
+}
+
+btnRace.addEventListener('click', () => {
+    let index = getRandomNumber(0, race.length-1)
+    result.innerText =race[index];
+});
